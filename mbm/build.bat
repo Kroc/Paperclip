b97ead87500030fc9c21717c1a5e380c7163e6f1
Kroc Camen <kroc@camendesign.com>
2020-11-02 09:51:09 +0000

    Allow building b&w / colour MBM file
    Defaults to colour for now until the top-level build script is updated to provide separate b&w / colour builds

--- mbm/build.bat
+++ mbm/build.bat
@@ -1,7 +1,13 @@
 @ECHO OFF
 PUSHD %~dp0
 
-bmconv "Paperclip.mbl"
+REM # colour mode, either "cl" for colour
+REM # or "bw" for black & white
+
+SET "CMODE=%~1"
+IF [%CMODE%] == [] SET "CMODE=cl"
+
+bmconv "Paperclip_%CMODE%.mbl"
 
 COPY /Y /B "Paperclip.mbm" "\EPOC32\Release\WINS\Deb\Z\System\Apps\Paperclip\"
 COPY /Y /B "Paperclip.mbm" "\EPOC32\Release\WINS\Rel\Z\System\Apps\Paperclip\"
